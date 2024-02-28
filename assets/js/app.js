540b45f51a8fe7c7443fe00b39a4bbba7158d613
William Beckley <wbeckley13@gmail.com>
2024-02-28 12:35:16 -0500

    updated imports

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,8 +29,8 @@ let liveSocket = new LiveSocket("/live", Socket, {
   params: {_csrf_token: csrfToken}
 })
 
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var map = leaflet.map('map').setView([51.505, -0.09], 13);
+leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
